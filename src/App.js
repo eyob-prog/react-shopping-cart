35e3d4b39f18f256ca68d82d04dc1f93913bbea2
biruk <biruktesfa757@gmail.com>
2021-03-26 13:42:12 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return(
+    <div className="grid_container">
+       <header>
+          <a href="/">react shopping cart</a>
+       </header>  
+       <main>
+          Product list
+       </main> 
+       <footer>
+          All right is reserved.
+       </footer>     
+    </div>
+  );
+}
+ export default App
